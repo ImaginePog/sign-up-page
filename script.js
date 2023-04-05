40ad3853fd2ee61b03d4ab6b42c9204ae890a4f1
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-04-05 22:05:27 +0545

    Style and add pass check

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert("hello world");
+// alert("hello world");
+
+const form = document.querySelector("form");
+const pass = document.querySelector("#pass-in");
+const confirmPass = document.querySelector("#pass-confirm-in");
+
+function checkPass() {
+	if (pass.value === confirmPass.value) {
+		if (pass.classList.contains("invalid")) {
+			pass.classList.remove("invalid");
+			confirmPass.classList.remove("invalid");
+
+			const spane = document.querySelector(".invalid-notif");
+			spane.remove();
+		}
+	} else {
+		if (!pass.classList.contains("invalid")) {
+			pass.classList.add("invalid");
+			confirmPass.classList.add("invalid");
+
+			const spane = document.createElement("span");
+			spane.textContent = "*Passwords dont match!";
+			spane.classList.add("invalid-notif");
+			pass.after(spane);
+		}
+	}
+}
+
+pass.addEventListener("focus", checkPass);
+pass.addEventListener("blur", checkPass);
+
+confirmPass.addEventListener("focus", checkPass);
+confirmPass.addEventListener("blur", checkPass);
